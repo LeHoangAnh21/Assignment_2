f797caa0dd524e3bf0dc942a69fba60f39973eb8
LeHoangAnh21 <84314151+LeHoangAnh21@users.noreply.github.com>
2021-06-30 12:42:44 +0700

    ok

--- app.js
+++ app.js
@@ -89,20 +89,25 @@ app.get('/insert',(req,res)=>{
 })
 
 app.post('/doInsert', async (req,res)=>{
+    let error = [];
+    let flag = true;
     const nameInput = req.body.txtName;
     const priceInput = req.body.txtPrice;
     const newProduct = {name:nameInput, price:priceInput};
-    // await dbHandler.insertOneIntoCollection(newProduct,"SanPham");
-    // res.render('home')
 
     if(!dbHandler.checkName(nameInput))
     {
-        res.render('insert',{nameError:'Please Enter Name Again!'})
-    }else if(dbHandler.checkName(priceInput)){
-        res.render('insert',{priceError:'Please Enter Price Again!'})
-    }else{  
+        error["name"] = 'Please Enter Name Again!'
+        flag = false;
+    }if(dbHandler.checkPrice(priceInput)){
+        error["price"] = 'Please Enter price Again!'
+        flag = false;
+    }if(flag == true){
         await dbHandler.insertOneIntoCollection(newProduct,"SanPham");
         res.render('home')
+    }
+    else{  
+        res.render('insert',{error:error})
     }
 })
 
